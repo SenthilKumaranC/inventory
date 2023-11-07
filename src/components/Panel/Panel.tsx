5f5a625d4e7b521d1181c0041f55b2f317e77901
SenthilKumaranC <mailtocskumaran@gmail.com>
2023-11-07 13:49:13 +0530

    added close button

--- src/components/Panel/Panel.tsx
+++ src/components/Panel/Panel.tsx
@@ -1,5 +1,23 @@
 import { useNavigate } from "react-router-dom"
 
+export const CloseIcon = () => {
+  return (
+    <svg
+      role="button"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        fillRule="evenodd"
+        d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-1.72 6.97a.75.75 0 10-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 101.06 1.06L12 13.06l1.72 1.72a.75.75 0 101.06-1.06L13.06 12l1.72-1.72a.75.75 0 10-1.06-1.06L12 10.94l-1.72-1.72z"
+        clipRule="evenodd"
+      />
+    </svg>
+  )
+}
+
 const Panel = (props: any) => {
   const cLen = props.children?.length
   const navigate = useNavigate()
@@ -19,7 +37,7 @@ const Panel = (props: any) => {
             onClick={() => navigate(-1)}
             className="text-black font-semibold flex justify-end"
           >
-            x
+            <CloseIcon></CloseIcon>
           </button>
         )}
       </div>
